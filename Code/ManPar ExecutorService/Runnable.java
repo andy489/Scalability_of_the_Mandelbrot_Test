fd047d4886485a45c0396d30784926ab2b279fa1
Andrey Stoev <akstoev@uni-sofia.bg>
2021-07-22 15:01:50 +0300

    Create Runnable.java

---
+++ Code/ManPar ExecutorService/Runnable.java
@@ -0,0 +1,42 @@
+import org.apache.commons.math3.complex.Complex;
+
+public class Runnable implements java.lang.Runnable {
+    private final int taskLocation;
+
+    public Runnable(int taskLocation) {
+        this.taskLocation = taskLocation;
+    }
+
+    private Complex calcNewValue(Complex z, Complex c) {
+        return z.multiply(z).add(c);
+    }
+
+    private short getIndex(Complex c) {
+        Complex z = new Complex(0, 0);
+
+        short curr_iter = -128;
+
+        while (z.abs() <= 2 && curr_iter < DynamicMandelbrot.MAX_ITERATIONS) {
+            z = calcNewValue(z, c);
+            curr_iter++;
+        }
+
+        return curr_iter;
+    }
+
+    @Override
+    public void run() {
+        int startFrom = taskLocation * DynamicMandelbrot.rowWidth;
+        int endTo = taskLocation * DynamicMandelbrot.rowWidth + DynamicMandelbrot.rowWidth;
+        for (int y = startFrom; y < endTo && y < DynamicMandelbrot.HEIGHT; y++) {
+            for (int x = 0; x < DynamicMandelbrot.WIDTH; x++) {
+                double pixel_x = DynamicMandelbrot.RE_START + ((double) x / DynamicMandelbrot.WIDTH) * (DynamicMandelbrot.RE_END - DynamicMandelbrot.RE_START);
+                double pixel_y = DynamicMandelbrot.IM_START + ((double) y / (DynamicMandelbrot.HEIGHT)) * (DynamicMandelbrot.IM_END - DynamicMandelbrot.IM_START);
+                Complex c = new Complex(pixel_x, pixel_y);
+
+                // Compute number of iterations // change comment
+                DynamicMandelbrot.indices[x][y] = (byte)getIndex(c);
+            }
+        }
+    }
+}
